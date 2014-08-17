db4680593ce506a2a748babded0cb108ff37c874
Kevin Spencer <kevin@kevinspencer.org>
2014-08-16 17:15:50 -0700

    adding distribution.pl

--- README.md
+++ README.md
@@ -2,3 +2,6 @@ flickr-tools
 ============
 
 Some kit I find handy for interacting with Flickr.
+
+distribution.pl
+faves-to-album.pl
---
+++ distribution.pl
@@ -0,0 +1,64 @@
+#!/usr/bin/env perl
+# Copyright 2014 Kevin Spencer <kevin@kevinspencer.org>
+#
+# Permission to use, copy, modify, distribute, and sell this software and its
+# documentation for any purpose is hereby granted without fee, provided that
+# the above copyright notice appear in all copies and that both that
+# copyright notice and this permission notice appear in supporting
+# documentation. No representations are made about the suitability of this
+# software for any purpose. It is provided "as is" without express or
+# implied warranty.
+#
+################################################################################
+
+use Data::Dumper;
+use File::HomeDir;
+use File::Spec;
+use Flickr::API2;
+use POSIX qw(ceil);
+use strict;
+use warnings;
+
+our $VERSION = '0.01';
+$Data::Dumper::Indent = 1;
+
+my $api_key_file = File::Spec->catfile(File::HomeDir->my_home(), '.flickr.key');
+my ($api_key, $api_secret) = retrieve_key_info();
+
+my $flickr = Flickr::API2->new({'key' => $api_key, secret => $api_secret});
+
+my $user   = $flickr->people->findByUsername('kevinspencer');
+my $info   = $user->getInfo();
+
+my $total_photos = $info->{photos}{count}{_content};
+
+# flickr.people.getPublicPhotos can handle 500 photos per 'page' request...
+my $photos_per_page = $total_photos >= 500 ? 500 : $total_photos;
+my $pages_needed    = ceil($total_photos / $photos_per_page);
+my $current_counter = $photos_per_page;
+
+my %distribution;
+for my $current_page_count (1..$pages_needed) {
+    my @photos = $user->getPublicPhotos(per_page => $photos_per_page, page => $current_page_count);
+    for my $photo (@photos) {
+        $distribution{$photo->{count_faves}}++;
+    }
+}
+
+print "Faves, Photos\n";
+for my $count (sort { $a <=> $b } keys(%distribution)) {
+    print "$count, $distribution{$count}\n";
+}
+
+sub retrieve_key_info {
+    if (-e $api_key_file) {
+        open(my $fh, '<', $api_key_file) || die "Could not read $api_key_file - $!\n";
+        my $api_key = <$fh>;
+        chomp($api_key);
+        my $api_secret = <$fh>;
+        chomp($api_secret);
+        close($fh);
+        return ($api_key, $api_secret);
+    }
+    return;
+}
