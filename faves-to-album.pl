9fb8d5291f7bfc38a2eb51a4693a1cfa0e243f74
Kevin Spencer <kevin@kevinspencer.org>
2014-09-01 07:56:51 -0700

    finding photos with threshold

--- faves-to-album.pl
+++ faves-to-album.pl
@@ -11,38 +11,69 @@
 #
 ################################################################################
 
-# experimental, Flickr::API2 on the CPAN doesn't support count_faves()
-use lib '/Users/kevin/code/Flickr-API2/lib';
 use Data::Dumper;
 use File::HomeDir;
 use File::Spec;
 use Flickr::API2;
+use Getopt::Long;
 use POSIX qw(ceil);
 use strict;
 use warnings;
 
-our $VERSION = '0.03';
+our $VERSION = '0.04';
 $Data::Dumper::Indent = 1;
+
+my $favorite_count_threshold;
+GetOptions("count=i" => \$favorite_count_threshold);
+if (! $favorite_count_threshold) {
+    print "Count not passed in, defaulting to 10...\n";
+    $favorite_count_threshold = 10;
+}
 
 my $api_key_file = File::Spec->catfile(File::HomeDir->my_home(), '.flickr.key');
 my ($api_key, $api_secret) = retrieve_key_info();
 
 my $flickr = Flickr::API2->new({'key' => $api_key, secret => $api_secret});
 
-my $user   = $flickr->people->findByUsername('kevinspencer');
-my $info   = $user->getInfo();
+main();
 
-my $total_photos = $info->{photos}{count}{_content};
+sub main {
+    my $photos_to_move = get_photos_above_threshold();
 
-# flickr.people.getPublicPhotos can handle 500 photos per 'page' request...
-my $photos_per_page = $total_photos >= 500 ? 500 : $total_photos;
-my $pages_needed    = ceil($total_photos / $photos_per_page);
-my $current_counter = $photos_per_page;
-for my $current_page_count (1..$pages_needed) {
-    my @photos = $user->getPublicPhotos(per_page => $photos_per_page, page => $current_page_count);
-    my $count = @photos;
-    print "$current_page_count => $current_counter => got $count photos back from the Flickrs\n";
-    $current_counter += $photos_per_page;
+    if (! $photos_to_move) {
+        print "Found no photos above threshold of $favorite_count_threshold, exiting.\n";
+        exit();
+    }
+
+    my $count = keys(%$photos_to_move);
+    my $photo_word = $count == 1 ? 'photo' : 'photos';
+    print "Found $count $photo_word above threshold of $favorite_count_threshold\n";
+
+    # TODO: check to see if we already have an album on Flickr called "$favorite_count_threshold or more" 
+}
+
+sub get_photos_above_threshold {
+    my $user   = $flickr->people->findByUsername('kevinspencer');
+    my $info   = $user->getInfo();
+
+    my $total_photos = $info->{photos}{count}{_content};
+
+    # flickr.people.getPublicPhotos can handle 500 photos per 'page' request...
+    my $photos_per_page = $total_photos >= 500 ? 500 : $total_photos;
+    my $pages_needed    = ceil($total_photos / $photos_per_page);
+    my $current_counter = $photos_per_page;
+    print "Checking Flickr for photos >= count threshold of $favorite_count_threshold ($total_photos photos)...\n";
+    my %photos_to_move;
+    for my $current_page_count (1..$pages_needed) {
+        my @photos = $user->getPublicPhotos(per_page => $photos_per_page, page => $current_page_count);
+        for my $photo (@photos) {
+            if ($photo->{count_faves} >= $favorite_count_threshold) {
+                $photos_to_move{$photo->{id}}{count} = $photo->{count_faves};
+                $photos_to_move{$photo->{id}}{title} = $photo->{title};
+            }
+        }
+    }
+    return %photos_to_move ? \%photos_to_move : undef;
 }
 
 sub retrieve_key_info {
